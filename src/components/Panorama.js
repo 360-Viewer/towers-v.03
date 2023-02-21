5151789c13301513f53dd85e18c91b239135f14f
Yusuf Arslan <yusufarsln98@outlook.com>
2023-02-21 10:45:48 +0300

    pano loaded experiment

--- src/components/Panorama.js
+++ src/components/Panorama.js
@@ -1,17 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
+import { AppContext } from "../App";
 import Controls from "./Controls";
 import styles from "./Panorama.module.css"
 
-export const loadPanorama = (psvRef, pano, panoData) => { }
+export const loadPanorama = (psvRef, pano, panoData, setPanoChanged) => {
+  setPanoChanged(true);
+  psvRef.current.setPanorama(pano, {
+    // transition: "cubic-bezier(0.25, 0.1, 0.25, 1)",
+    showLoader: false,
+    panoData: panoData,
+  }).then(() => {
+    setPanoChanged(false);
+  });
+}
 
 function Panorama({ psvRef, pano, panoData, setIsLoaded }) {
+  const appContext = useContext(AppContext);
+  const { panoChanged } = appContext;
 
   return (
-    <>
+    <div className={`${styles.container} ${panoChanged ? styles.blurred : ""}`}>
       <ReactPhotoSphereViewer
         ref={psvRef}
-        container={styles.container}
         loadingImg={null}
         loadingTxt={null}
         width={"100%"}
@@ -23,7 +34,7 @@ function Panorama({ psvRef, pano, panoData, setIsLoaded }) {
         panoData={panoData}
       ></ReactPhotoSphereViewer>
       <Controls psvRef={psvRef} />
-    </>
+    </div>
   )
 }
 
--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,17 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Panorama from '../components/Panorama';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import Panorama, { loadPanorama } from '../components/Panorama';
 import { panos } from '../assets/constants'
 import styles from "../styles/Tour.module.css";
 import Menu from '../components/Menu';
+import { AppContext } from '../App';
 
 
 function Tour() {
+  const appContext = useContext(AppContext);
+  const { setPanoChanged } = appContext;
+
   const psvRef = useRef(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [panoData, setPanoData] = useState(null);
 
+  const nextPano = () => {
+    loadPanorama(psvRef, panos["A-Block"]["L100"]["Night"], panoData, setPanoChanged);
+  }
+
   return (
     <div className={styles.tour}>
+      <button style={{ position: "absolute", top: 0, left: 0, zIndex: 1000 }} onClick={nextPano}>
+        change
+      </button>
       <Panorama
         psvRef={psvRef}
         pano={panos["A-Block"]["L100"]["Day"]}
